2cd9e3c0f27668f88d19d9afad55b0a07c3f6882
georapbox <georapbox@gmail.com>
2018-10-14 10:13:37 +0300

    workaround for UMD modules not loading in Node

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     filename: mode === 'development' ? `${libraryName}.js` : `${libraryName}.min.js`,
     library: libraryName,
     libraryTarget: libraryTarget || 'umd',
+    globalObject: '(typeof self !== \'undefined\' ? self : this)', // TODO Hack (for Webpack 4+) to enable create UMD build which can be required by Node without throwing error for window being undefined (https://github.com/webpack/webpack/issues/6522)
     umdNamedDefine: true
   },
   module: {
